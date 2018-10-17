7d591a40ed91e691327a5d1dd2a860b22a0a989d
Ben <benharvie94@gmail.com>
2018-10-17 14:40:00 +0100

    Petrol tests passed

--- src/test/java/PetrolTest.java
+++ src/test/java/PetrolTest.java
@@ -1,2 +1,40 @@
+import Vehicles.Petrol;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PetrolTest {
+
+    Petrol petrol;
+
+    @Before
+    public void before() {
+        petrol = new Petrol("Toyota", "Prius", 4300.00, "Pink", 1.4);
+    }
+
+    @Test
+    public void hasMake() {
+        assertEquals("Toyota", petrol.getMake());
+    }
+
+    @Test
+    public void hasModel() {
+        assertEquals("Prius", petrol.getModel());
+    }
+
+    @Test
+    public void hasPrice() {
+        assertEquals(4300.00, petrol.getPrice(), 0.01);
+    }
+
+    @Test
+    public void hasColour() {
+        assertEquals("Pink", petrol.getColour());
+    }
+
+    @Test
+    public void hasEngineSize() {
+        assertEquals(1.4, petrol.getEngineSize(), 0.01);
+    }
 }
