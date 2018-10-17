96dc8f203d8f5aee15bc1b4f34660ff1f5a5e496
Ben <benharvie94@gmail.com>
2018-10-17 14:28:54 +0100

    Tyre class created and tests passing

--- src/main/java/Components/Tyre.java
+++ src/main/java/Components/Tyre.java
@@ -1,4 +1,15 @@
 package Components;
 
-public class Tyre {
+public class Tyre extends Component{
+
+    private double size;
+
+    public Tyre(String make, String model, double size) {
+        super(make, model);
+        this.size = size;
+    }
+
+    public double getSize() {
+        return size;
+    }
 }
--- src/test/java/TyreTest.java
+++ src/test/java/TyreTest.java
@@ -1,2 +1,20 @@
+import Components.Tyre;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TyreTest {
+
+    Tyre tyre;
+
+    @Before
+    public void before() {
+        tyre = new Tyre("Tyre1", "VW", 18.00);
+    }
+
+    @Test
+    public void hasSize() {
+        assertEquals(18.00, tyre.getSize(), 0.01);
+    }
 }
