dd68bb2a11980da3f1c6fe189251b9d25adc2717
Ben <benharvie94@gmail.com>
2018-10-17 15:12:14 +0100

    Customer basic getters tests passing

--- src/main/java/CodeClanCars/Customer.java
+++ src/main/java/CodeClanCars/Customer.java
@@ -20,7 +20,7 @@ public class Customer {
         return wallet;
     }
 
-    public boolean isDrivingLicense() {
+    public boolean hasDrivingLicense() {
         return drivingLicense;
     }
 }
--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -1,2 +1,31 @@
+import CodeClanCars.Customer;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
 public class CustomerTest {
+    Customer customer;
+
+    @Before
+    public void before() {
+        customer = new Customer("Bob", 10000, true);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Bob", customer.getName());
+    }
+
+    @Test
+    public void hasWallet() {
+        assertEquals(10000, customer.getWallet(), 0.01);
+    }
+
+    @Test
+    public void hasDrivingLicense() {
+        assertTrue(customer.hasDrivingLicense());
+    }
 }
