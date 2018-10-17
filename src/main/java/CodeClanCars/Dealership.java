4578ce3c01d0901753232d85bbe06d2cb4d6f209
Ben <benharvie94@gmail.com>
2018-10-17 14:54:47 +0100

    Dealership class created

--- src/main/java/CodeClanCars/Dealership.java
+++ src/main/java/CodeClanCars/Dealership.java
@@ -1,4 +1,34 @@
 package CodeClanCars;
 
+import Vehicles.Petrol;
+import Vehicles.Vehicle;
+import java.util.ArrayList;
+
 public class Dealership {
+
+    private String name;
+    private ArrayList<Vehicle> vehicles;
+    private double till;
+
+    public Dealership(String name){
+        this.name = name;
+        this.vehicles = new ArrayList<>();
+        this.till = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Vehicle> getVehicles() {
+        return vehicles;
+    }
+
+    public double getTill() {
+        return till;
+    }
+
+    public void addCar(Vehicle car) {
+        vehicles.add(car);
+    }
 }
--- src/test/java/DealershipTest.java
+++ src/test/java/DealershipTest.java
@@ -1,2 +1,45 @@
+import CodeClanCars.Dealership;
+import Vehicles.Electric;
+import Vehicles.Hybrid;
+import Vehicles.Petrol;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class DealershipTest {
+
+    Dealership dealership;
+    Petrol car1;
+    Hybrid car2;
+    Electric car3;
+
+    @Before
+    public void before() {
+        car1 = new Petrol("Toyota", "Prius", 4300.00, "Pink", 1.4);
+        car2 = new Hybrid("Toyota", "Prius", 4300.00, "Pink", 1.4);
+        car3 = new Electric("Toyota", "Prius", 4300.00, "Pink", 1.4);
+
+        dealership = new Dealership("CodeClan Cars" );
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("CodeClan Cars", dealership.getName());
+    }
+
+    @Test
+    public void hasNoCars() {
+        assertEquals(0, dealership.getVehicles().size());
+    }
+
+    @Test
+    public void addsCars() {
+        dealership.addCar(car1);
+        dealership.addCar(car2);
+        dealership.addCar(car3);
+        assertEquals(3, dealership.getVehicles().size());
+    }
 }
