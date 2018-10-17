f5779aa7f4d15d08017633b937ea5bf3b2d18838
Ben <benharvie94@gmail.com>
2018-10-17 14:37:21 +0100

    Electric tests passed

--- src/test/java/ElectricTest.java
+++ src/test/java/ElectricTest.java
@@ -1,2 +1,40 @@
+import Vehicles.Electric;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ElectricTest {
+
+    Electric electric;
+
+    @Before
+    public void before() {
+        electric = new Electric("Toyota", "Prius", 4300.00, "Pink", 1.4);
+    }
+
+    @Test
+    public void hasMake() {
+        assertEquals("Toyota", electric.getMake());
+    }
+
+    @Test
+    public void hasModel() {
+        assertEquals("Prius", electric.getModel());
+    }
+
+    @Test
+    public void hasPrice() {
+        assertEquals(4300.00, electric.getPrice(), 0.01);
+    }
+
+    @Test
+    public void hasColour() {
+        assertEquals("Pink", electric.getColour());
+    }
+
+    @Test
+    public void hasEngineSize() {
+        assertEquals(1.4, electric.getEngineSize(), 0.01);
+    }
 }
